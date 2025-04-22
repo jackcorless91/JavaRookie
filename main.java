b24ce86173b31e97f52c023db8f09ba21c3681a1
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-04-22 19:41:37 +1000

    data types

--- main.java
+++ main.java
@@ -1,8 +1,45 @@
 public class main {
 
-
+//needed for code to run
     public static void main(String[] args){
 
+        System.out.println("I like pizza");
+        System.out.println("It's really good");
+        System.out.println("Buy me pizza");
+
+
+//        Variables = a reusable container for a value
+//                    a variable behaves as if it was the value it contains
+
+//        primitive = simple value stored directly in memory (stack)
+//        reference = memory address (stack) that points to the (heap)
+
+//        primitive: int, double (not whole numbers), char (single character), boolean
+//        reference: string, array, object
+
+//        2 steps to creating a valiable, 1. declaration, 2. asssignment
+
+//        camelCase for variable names
+
+        int age = 21;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = 23.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        String name = "Butt";
+
+        System.out.println(name);
     }
 }
 
